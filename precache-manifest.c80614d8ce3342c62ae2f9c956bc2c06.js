7958925df06ccdfaa94d2c9f0d6499fa4df86ddd
Crewzul12 <zulhresident12@gmail.com>
2020-02-18 23:30:09 +0800

    Updates

---
+++ precache-manifest.c80614d8ce3342c62ae2f9c956bc2c06.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "453e61db5cccce38167cb3dec1db8100",
+    "url": "/robotapi/index.html"
+  },
+  {
+    "revision": "dd0c85b2fe38447044f9",
+    "url": "/robotapi/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "3d8a9a75e91627582919",
+    "url": "/robotapi/static/css/main.ee76fcff.chunk.css"
+  },
+  {
+    "revision": "dd0c85b2fe38447044f9",
+    "url": "/robotapi/static/js/2.3f90059f.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/robotapi/static/js/2.3f90059f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3d8a9a75e91627582919",
+    "url": "/robotapi/static/js/main.1f62eeda.chunk.js"
+  },
+  {
+    "revision": "3ea230ec4fb4f9a620f5",
+    "url": "/robotapi/static/js/runtime-main.6b2b1975.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robotapi/static/media/SEGA.12a3db8b.woff"
+  }
+]);
